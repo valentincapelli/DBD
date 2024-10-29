36cba7bb400074940c15672046e554c21a3e3df1
Valentin Capelli <valentin.capelli@hotmail.com>
2024-10-29 09:13:58 -0300

    Practica

--- Practicas/Practica4/ejercicio3.sql
+++ Practicas/Practica4/ejercicio3.sql
@@ -4,4 +4,78 @@ Integrante = (DNI, nombre, apellido, dirección, email, fecha_nacimiento, codigo
 Escenario = (nroEscenario, nombre_escenario, ubicación, cubierto, m2, descripción)
 Recital = (fecha, hora, nroEscenario (fk), codigoB (fk)) */
 
-1_ 
+/*1. Listar DNI, nombre, apellido,dirección y email de integrantes nacidos entre 1980 y 1990 y que
+hayan realizado algún recital durante 2023.*/
+
+SELECT i.DNI, i.nombre, i.apellido, i.dirección, i.email
+FROM integrante i NATURAL JOIN banda b NATURAL JOIN Recital r 
+WHERE (i.fecha_nacimiento BETWEEN '01/01/1980' AND '31/12/1990' AND r.fecha BETWEEN '01/01/2023' AND '31/12/2023')
+
+/* 2. Reportar nombre, género musical y año de creación de bandas que hayan realizado recitales
+durante 2023, pero no hayan tocado durante 2022.*/
+
+SELECT b.nombreBanda, b.genero_musical, b.año_creacion
+FROM banda b NATURAL JOIN recital r 
+WHERE (r.fecha BETWEEN '01/01/2023' AND '31/12/2023') 
+EXCEPT(
+    SELECT b.nombreBanda, b.genero_musical, b.año_creacion
+    FROM banda b NATURAL JOIN recital r 
+    WHERE (r.fecha BETWEEN '01/01/2022' AND '31/12/2022') 
+)
+
+/*3. Listar el cronograma de recitales del día 04/12/2023. Se deberá listar nombre de la banda que
+ejecutará el recital, fecha, hora, y el nombre y ubicación del escenario correspondiente.*/
+
+SELECT b.nombreBanda, r.fecha, r.hora, e.nombre_escenario, e.ubicación
+FROM banda b NATURAL JOIN recital r NATURAL JOIN escenario e 
+WHERE (r.fecha = '04/12/2023')
+
+/*4. Listar DNI, nombre, apellido,email de integrantes que hayan tocado en el escenario con nombre
+‘Gustavo Cerati’ y en el escenario con nombre ‘Carlos Gardel’.*/
+
+SELECT i.DNI, i.nombre, i.apellido, i.email 
+FROM integrante i NATURAL JOIN banda b NATURAL JOIN recital r NATURAL JOIN escenario e 
+WHERE (e.nombre_escenario = 'Gustavo Cerati')
+INTERSECT(
+    SELECT i.DNI, i.nombre, i.apellido, i.email 
+    FROM integrante i NATURAL JOIN banda b NATURAL JOIN recital r NATURAL JOIN escenario e 
+    WHERE (e.nombre_escenario = 'Carlos Gardel')
+)
+
+/*5. Reportar nombre, género musical y año de creación de bandas que tengan más de 8 integrantes.*/
+
+SELECT b.nombre, b.genero_musical, b.año_creacion
+FROM banda b NATURAL JOIN integrante i 
+GROUP BY b.codigoB, b.nombre, b.genero_musical, b.año_creacion
+HAVING COUNT(i.DNI) > 8
+
+/*6. Listar nombre de escenario, ubicación y descripción de escenarios que solo tuvieron recitales
+con el género musical rock and roll. Ordenar por nombre de escenario*/
+
+SELECT e.nombre_escenario, e.ubicación, e.descripción
+FROM Escenario e NATURAL JOIN Recital r NATURAL JOIN Banda b 
+WHERE b.genero_musical = 'rock and roll' 
+EXCEPT (
+    SELECT e.nombre_escenario, e.ubicación, e.descripción
+    FROM Escenario e NATURAL JOIN Recital r NATURAL JOIN Banda b
+    WHERE b.genero_musical <> 'rock and roll'
+)
+ORDER BY e.nombre_escenario
+
+/*7. Listar nombre, género musical y año de creación de bandas que hayan realizado recitales en
+escenarios cubiertos durante 2023.// cubierto es true, false según corresponda*/
+
+SELECT b.nombreBanda, b.genero_musical, b.año_creacion
+FROM banda b NATURAL JOIN recital r NATURAL JOIN escenario e 
+WHERE (r.fecha BETWEEN '01/01/2023' AND '31/12/2023') AND (e.cubierto = true)
+
+/*8. Reportar para cada escenario, nombre del escenario y cantidad de recitales durante 2024*/
+
+SELECT e.nombre_escenario, COUNT(*) as CANTIDAD_DE_RECITALES
+FROM recital r NATURAL JOIN escenario e 
+WHERE (r.fecha BETWEEN '01/01/2024' AND '31/12/2024')
+GROUP BY e.nroEscenario, e.nombre_escenario
+
+/*9. Modificar el nombre de la banda ‘Mempis la Blusera’ a: ‘Memphis la Blusera’.*/
+
+UPDATE banda SET nombreBanda = 'Memphis la Blusera' WHERE b.nombreBanda = 'Mempis la Blusera'
---
+++ Practicas/Practica4/ejercicio4.sql
@@ -0,0 +1,80 @@
+/*Ejercicio 4
+PERSONA = (DNI, Apellido, Nombre, Fecha_Nacimiento, Estado_Civil, Genero)
+ALUMNO = (DNI (fk), Legajo, Año_Ingreso)
+PROFESOR = (DNI (fk), Matricula, Nro_Expediente)
+TITULO = (Cod_Titulo, Nombre, Descripción)
+TITULO-PROFESOR = (Cod_Titulo (fk), DNI (fk), Fecha)
+CURSO = (Cod_Curso, Nombre, Descripción, Fecha_Creacion, Duracion)
+ALUMNO-CURSO = (DNI (fk), Cod_Curso (fk), Año, Desempeño, Calificación)
+PROFESOR-CURSO = (DNI (fk), Cod_Curso (fk), Fecha_Desde, Fecha_Hasta) */
+
+--Consultar natural join y asterisco cuando hacemos un count
+
+/*1. Listar DNI, legajo y apellido y nombre de todos los alumnos que tengan año de ingreso inferior a
+2014.*/
+
+SELECT a.DNI, a.legajo, p.nombre, p.apellido 
+FROM Persona p NATURAL JOIN Alumno a 
+WHERE ( a.anio_ingreso < 1/1/2014)
+
+/*2. Listar DNI, matrícula, apellido y nombre de los profesores que dictan cursos que tengan más de
+100 horas de duración. Ordenar por DNI.*/
+
+SELECT pro.DNI, pro.matricula, p.apellido, p.nombre 
+FROM Persona p NATURAL JOIN Profesor pro NATURAL JOIN Profesor-curso pc NATURAL JOIN Curso c
+WHERE (c.duracion > 100)
+ORDER BY pro.DNI 
+ 
+/*3. Listar el DNI, Apellido, Nombre, Género y Fecha de nacimiento de los alumnos inscriptos al
+curso con nombre “Diseño de Bases de Datos” en 2023*/
+
+SELECT p.DNI, p.apellido, p.nombre, p.genero, p.fecha_nacimiento
+FROM Persona p NATURAL JOIN Alumno a NATURAL JOIN Alumno_curso ac INNER JOIN Curso c ON (ac.Cod_Curso = c.Cod_Curso)
+WHERE (c.nombre = 'Diseño de Bases de Datos' AND ac.Anio = 2023)
+
+/*Listar el DNI, Apellido, Nombre y Calificación de aquellos alumnos que obtuvieron una
+calificación superior a 8 en algún curso que dicta el profesor “Juan Garcia”. Dicho listado deberá
+estar ordenado por Apellido y nombre*/
+
+SELECT p.DNI, p.Apellido, p.Nombre, ac.Calificacion
+FROM  Persona p NATURAL JOIN ALUMNO_CURSO ac 
+WHERE (ac.Calificacion > 8) AND (ac.Cod_Curso IN (
+    SELECT pc.Cod_Curso 
+    FROM PROFESOR_CURSO pc NATURAL JOIN Persona p1
+    WHERE (p1.Nombre = 'Juan' AND p1.Apellido = 'Garcia'))) 
+ORDER BY p.Apellido, p.Nombre 
+
+
+/*5. Listar el DNI, Apellido, Nombre y Matrícula de aquellos profesores que posean más de 3 títulos.
+Dicho listado deberá estar ordenado por Apellido y Nombre*/
+
+SELECT p.DNI, p.Apellido, p.Nombre, pro.Matricula 
+FROM PERSONA p NATURAL JOIN PROFESOR pro NATURAL JOIN TITULO_PROFESOR tp
+GROUP BY p.DNI, p.Apellido, p.Nombre, pro.Matricula
+HAVING COUNT (*) > 3
+ORDER BY p.Apellido, p.Nombre  
+
+
+/*6. Listar el DNI, Apellido, Nombre, Cantidad de horas y Promedio de horas que dicta cada profesor.
+La cantidad de horas se calcula como la suma de la duración de todos los cursos que dicta*/
+
+SELECT p.DNI, p.Apellido, p.Nombre, SUM (c.Duracion) AS cantHoras, AVG(c.Duracion) AS promedioHoras
+FROM Persona p NATURAL JOIN PROFESOR_CURSO pc INNER JOIN CURSO c ON (pc.Cod_Curso = c.Cod_Curso)
+GROUP BY p.DNI, p.Apellido, p.Nombre 
+
+/*7. Listar Nombre y Descripción del curso que posea más alumnos inscriptos y del que posea
+menos alumnos inscriptos durante 2024*/ 
+
+SELECT MAX( 
+    SELECT c.Nombre, c.Descripcion 
+FROM CURSO c NATURAL JOIN Alumno_Curso ac 
+WHERE (ac.Anio BETWEEN 1/1/2024 AND 31/12/2024)
+GROUP BY c.Cod_Curso, c.Nombre, c.Descripcion
+HAVING COUNT(ac.DNI)
+), MIN (
+    SELECT c.Nombre, c.Descripcion 
+FROM CURSO c NATURAL JOIN Alumno_Curso ac 
+WHERE (ac.Anio BETWEEN 1/1/2024 AND 31/12/2024)
+GROUP BY c.Cod_Curso, c.Nombre, c.Descripcion
+HAVING COUNT(ac.DNI)
+)
